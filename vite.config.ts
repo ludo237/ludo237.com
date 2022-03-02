4324a4dbbd8cd7675754d67ee55877685160708c
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2022-03-02 16:53:47 +0100

    Specify a different base for Gitlab pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/ludo237.com/" : "/",
   publicDir: "static",
   plugins: [vue(), vueJsx()],
   resolve: {
