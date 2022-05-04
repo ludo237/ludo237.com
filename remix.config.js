3d4f33d4d5eab4446ac1f1cd18a5de103bcbf635
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2022-05-04 12:56:05 +0200

    Bump updates and add vercel.json file
    Signed-off-by: Claudio Ludovico Panetta <ludovico1990@hotmail.it>

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,12 @@
 /**
- * @type {import('@remix-run/dev').AppConfig}
+ * @type {import("@remix-run/dev").AppConfig}
  */
 module.exports = {
   ignoredRouteFiles: [".*"],
-  // appDirectory: "app",
+  appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
   // devServerPort: 8002
 };
