36351622f108ec65d38e3b5dec194add31d66173
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2022-05-04 15:29:07 +0200

    Add specific vercel configs
    Signed-off-by: Claudio Ludovico Panetta <ludovico1990@hotmail.it>

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,12 @@
  * @type {import("@remix-run/dev").AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "vercel",
   ignoredRouteFiles: [".*"],
   appDirectory: "app",
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // devServerPort: 8002
