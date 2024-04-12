be8f6e87bbb8819ca0768b4024e9175b5c4d54f3
Claudio Ludovico <fresh.bell8525@fastmail.com>
2024-04-12 08:46:30 +0200

    fix: missing package name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizePackageImports: ['package-name'],
+    optimizePackageImports: ['lucide-react'],
   },
   images: {
     remotePatterns: [
