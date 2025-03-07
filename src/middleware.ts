1cbf8702c8ff57cb0fb0b82f7fd4767b332db99e
Claudio Ludovico <fresh.bell8525@fastmail.com>
2025-03-07 12:44:05 +0100

    chore: remove unused code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,7 @@
 import type { NextRequest } from 'next/server';
-import { updateSession } from '~/lib/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  return request;
 }
 
 export const config = {
