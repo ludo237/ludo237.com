a77c4bbae77ed1058b8ecb9eebae1a938522db7b
Claudio Ludovico <fresh.bell8525@fastmail.com>
2025-07-24 16:07:18 +0200

    fix: forcing https
    Signed-off-by: Claudio Ludovico <fresh.bell8525@fastmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,21 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        if ($this->app->environment(['production', 'prod'])) {
+            URL::forceScheme('https');
+        }
+
+        Date::use(CarbonImmutable::class);
+    }
 
     public function boot(): void {}
 }
