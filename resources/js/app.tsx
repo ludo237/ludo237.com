55d81ec148340452a0b5695e7060782c1c1b6086
Claudio Ludovico <fresh.bell8525@fastmail.com>
2025-07-24 17:01:14 +0200

    fix: favicon, build process, key generation
    Signed-off-by: Claudio Ludovico <fresh.bell8525@fastmail.com>

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,7 +5,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Ludo237';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
